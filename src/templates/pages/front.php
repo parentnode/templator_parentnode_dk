d31de6266e732ad97d54424183207c0ec269c671
kaestel <kaestel@e-types.com>
2014-03-25 14:32:42 +0100

    updated base

---
+++ src/templates/pages/front.php
@@ -0,0 +1,49 @@
+<div class="scene">
+
+	<div class="section??">
+		<h1>WhatTheFramework - It's so simple</h1>
+
+		<div class="c150">
+			<p>
+				WTF is a SEO optimized Markup model for HTML, that allows you to build any kind of site or app, 
+				for any kind of device. It is fully W3C- and AAA-compliant, lightweight and as flexible as you will ever need. 
+			</p>
+			<p>
+				It is a set of bestpractices deducected from more than a decade of working professionally with HTML.
+			</p>
+			<p>
+				It is really easy to implement and works with any kind of modern CMS', Drupal, Sitecore or Umbraco among others.
+				You can reuse any snippet og HTML across websites with different designs and easily implement you current design in a new CMS.
+				Or you can skin your content diffently for diffrent consumers and devices. One markup, multiple presentations.
+			</p>
+		</div>
+
+		<div class="c150">
+			<div class="promotion">
+				<h2>Advanteges of WTF?</h2>
+				<ul>
+					<li>Your markup works seamlessly in all HTML browsers.</li>
+					<li>Complete separation of Content, Design and Functionality.</li>
+					<li>It allows you to finish your clean HTML templates in a matter or hours, not days.</li>
+					<li>Implementation is faster, because the HTML is so simple.</li>
+					<li>You do not have to think about SEO, because your markup is semantic by nature.</li>
+					<li>Update the interface without changing your markup.</li>
+					<li>Build multiple fully optimized layouts/interfaces using <a href="/segments">Segments</a></li>
+				</ul>
+				<ul class="actions">
+					<li class="more"><a href="/principles" class="button primary">How is all this possible?</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="section">
+		<div class="promotion">
+			<h3>Want to contribute?</h3>
+			<p>
+				We always need help working on WTF. Send an email to 
+				<a href="mailto:contribute@whattheframework.org">contribute@whattheframework.org</a> to join the team.
+			</p>
+		</div>
+	</div>
+</div>
