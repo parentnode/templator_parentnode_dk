00efe91875bc5f2d627705b8d8627dd338283a56
kaestel <kaestel@e-types.com>
2014-04-05 22:16:51 +0200

    merged and added tracking

--- src/www/js/seg_mobile.js
+++ src/www/js/seg_mobile.js
@@ -12,8 +12,8 @@ if(!u || !Util) {
 
 
 /*ga.js*/
-u.ga_account = 'UA-49739795-1';
-u.ga_domain = 'janitor.parentnode.dk';
+u.ga_account = 'UA-49741763-1';
+u.ga_domain = 'modulator.parentnode.dk';
 
 
 /*u-googleanalytics.js*/
