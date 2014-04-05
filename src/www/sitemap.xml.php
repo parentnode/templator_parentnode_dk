7d6933b1253774a9f7659d09be2d3534326ba958
kaestel <kaestel@e-types.com>
2014-04-05 22:40:10 +0200

    path

--- src/www/sitemap.xml.php
+++ src/www/sitemap.xml.php
@@ -10,7 +10,7 @@ print '<?xml version="1.0" encoding="UTF-8"?>';
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc>http://janitor.parentnode.dk/</loc>
+		<loc>http://modulator.parentnode.dk/</loc>
 		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/front.php")) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>1</priority>
