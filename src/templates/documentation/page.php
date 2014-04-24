a1b76996d63605e3676b03f5df87b76e3b3fb92e
StefanFriedli <friedlistephan@gmail.com>
2014-04-24 12:35:27 +0200

    update

--- src/templates/documentation/page.php
+++ src/templates/documentation/page.php
@@ -18,7 +18,11 @@
 &lt;div id=&quot;page&quot;&gt;
 
 	&lt;div id=&quot;header&quot;&gt;
-
+		&lt;ul class=&quot;servicenavigation&quot;&gt;
+			&lt;li class=&quot;keynav navigation&quot;&gt;&lt;a href=&quot;#navigation&quot; ref=&quot;nofollow&quot;&gt;To navigation&lt;/a&gt;&lt;/li&gt;
+			&lt;li class=&quot;keynav search&quot;&gt;&lt;a href=&quot;/search&quot; ref=&quot;search&quot;&gt;Search&lt;/a&gt;&lt;/li&gt;
+			&lt;li class=&quot;keynav help&quot;&gt;&lt;a href=&quot;/help&quot; ref=&quot;help&quot;&gt;Help&lt;/a&gt;&lt;/li&gt;
+		&lt;/ul&gt;
 	&lt;/div&gt;
 
 	&lt;div id=&quot;content&quot;&gt;&lt;/div&gt;
@@ -26,14 +30,15 @@
 	&lt;div id=&quot;navigation&quot;&gt;&lt;/div&gt;
 
 	&lt;div id=&quot;footer&quot;&gt;
-
+		&lt;ul class=&quot;servicenavigation&quot;&gt;
+			&lt;li class=&quot;keynav terms&quot;&gt;&lt;a href=&quot;/terms&quot;&gt;Terms&lt;/a&gt;&lt;/li&gt;
+		&lt;/ul&gt;
 	&lt;/div&gt;
 
 &lt;/div&gt;
 
 &lt;/body&gt;
-&lt;/html&gt;
-	</code>
+&lt;/html></code>
 
 
 
