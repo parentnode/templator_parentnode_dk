ffc16b431828104182f525e01ec268d0730822ec
kaestel <kaestel@e-types.com>
2014-06-04 10:53:17 +0200

    spellchecked

--- src/templates/pages/front.php
+++ src/templates/pages/front.php
@@ -2,20 +2,23 @@
 
 	<h1>It's so simple when you stop making it complicated</h1>
 	<p>
-		Please be patient - this is work in progress.
-	</p>
-	<p>
-		Modulator is a SEO optimized Markup model for HTML, that allows you to build any kind of site or app, 
+		Modulator is a SEO optimized markup model for HTML, that allows you to build any kind of site or app, 
 		for any kind of device. It is fully W3C- and AAA-compliant, lightweight and as flexible as you will ever need. 
+		This markup model is per default supported by all HTML capable browsers - finally there is a way
+		to spread your message across the planet, regardless of enduser device. That includes the more than 6.000.000.000
+		mobilephones. The only question left is: What do you want to say?
 	</p>
 	<p>
-		It is a set of bestpractices deducected from more than a decade of working professionally with HTML.
+		Modulator is a set of bestpractices deductected from more than a decade of working professionally with HTML,
+		always striving towards a complete separation of content, design and functionality and full accessibility
+		for both the living and the binary.
 	</p>
 	<p>
-		It is really easy to implement and works with any kind of modern CMS'.
-		You can reuse any snippet og HTML across websites with different designs and easily implement you current 
-		design in a new CMS. Or you can skin your content diffently for diffrent consumers and devices. One 
-		markup, multiple presentations.
+		It is really easy to implement and works with any modern CMS. Ironically in it's attempt to prioritize frontend
+		development it ended up making backend implementation easier than ever.
+		You can reuse any snippet of HTML across websites with different designs and easily implement you current 
+		design and markup in a new CMS. Or you can skin your content diffently for different consumers and devices. One 
+		markup, multiple presentations. It really IS that simple.
 	</p>
 
 	<h2>Advanteges of Modulator</h2>
@@ -26,7 +29,16 @@
 		<li>Implementation is faster, because the HTML is so simple.</li>
 		<li>You do not have to think about SEO, because your markup is semantic by nature.</li>
 		<li>Update the interface without changing your markup.</li>
+		<li>Fully <a href="http://schema.org" target="_blank">Schema.org</a> compliant.</li>
 	</ul>
+
+	<p>
+		Modulator is part of the <a href="http://parentnode.dk" target="_blank">parentNode</a> family.
+	</p>
+
+	<p class="note">
+		Please be patient - The full documentation will be published during summer 2014.
+	</p>
 
 	<h2>Want to contribute?</h2>
 	<p>
