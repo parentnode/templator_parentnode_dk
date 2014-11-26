48c250cdfdf5156ea6837612daaf3f75b2208597
kaestel <kaestel@e-types.com>
2014-11-26 01:00:08 +0000

    Updated markup and color scheme

--- src/templates/pages/front.php
+++ src/templates/pages/front.php
@@ -1,51 +1,63 @@
-<div class="scene">
+<div class="scene i:front">
 
-	<h1>It's so simple <br />when you stop <br />making it complicated</h1>
-	<p>
-		Modulator is a SEO optimized markup model for HTML, that allows you to build any kind of site or app, 
-		for any kind of device. It is fully W3C- and AAA-compliant, lightweight and as flexible as you will ever need. 
-		The Modulator model is based on a set of bestpractices deducted over more than a decade of working 
-		professionally with HTML, CSS and JavaScript, prioritizing a complete separation of content, design and 
-		functionality and full accessibility for both the living and the binary.
-	</p>
-	<p>
-		Modulator markup is supported by all HTML capable browsers allowing you to spread your message 
-		across the planet, regardless of enduser device. That includes more than 6.000.000.000
-		mobilephones. The only question left is: <strong>What do you want to say?</strong>
-	</p>
+	<div class="article" itemscope itemtype="http://schema.org/Article">
+		<h1 itemprop="name">It's so simple <br />when you stop <br />making it complicated</h1>
 
-	<h2>Beauty of simplicity</h2>
-	<p>
-		The real beauty of this markup model is it's simplicity. It is just doing what you already know, and forgetting
-		those hacks that you have had to acquire along the way. You do not need them anymore. Modulator is really easy 
-		to implement and works with any modern frontend friendly CMS. It's simplistic and semantic structure makes it easy 
-		to read and implement. You can reuse any Modulator snippet across different websites and designs, apply 
-		functionality as needed. Modulator is perfect for both custom and template based websites.
-	</p>
-	<p>
-		 One markup, multiple presentations. This markup is for everyone and everything. It really IS that simple. 
-	</p>
+		<dl class="info">
+			<dt class="published_at">Date published</dt>
+			<dd class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", filemtime(__FILE__)) ?>"><?= date("Y-m-d, H:i", filemtime(__FILE__)) ?></dd>
+			<dt class="author">Author</dt>
+			<dd class="author" itemprop="author">Martin Kæstel Nielsen</dd>
+		</dl>
 
-	<h2>Advantages of Modulator</h2>
-	<ul>
-		<li>Your markup works seamlessly in all HTML browsers.</li>
-		<li>Complete separation of Content, Design and Functionality.</li>
-		<li>Finish your clean HTML templates in a matter or hours, not days.</li>
-		<li>Your markup is semantic by nature.</li>
-		<li>Implementation is faster.</li>
-		<li>Frontend is detached from backend development.</li>
-		<li>Update the interface without changing your markup.</li>
-		<li>Fully <a href="http://en.wikipedia.org/wiki/HTML5" target="_blank">HTML5</a> and <a href="http://schema.org" target="_blank">Schema.org</a> compliant.</li>
-	</ul>
+		<div class="articlebody" itemprop="articleBody">
+			<p>
+				Modulator is a SEO optimized markup model for HTML, that allows you to build any kind of site or app, 
+				for any kind of device. It is fully W3C- and AAA-compliant, lightweight and as flexible as you will ever need. 
+				The Modulator model is based on a set of bestpractices deducted over more than a decade of working 
+				professionally with HTML, CSS and JavaScript, prioritizing a complete separation of content, design and 
+				functionality and full accessibility for both the living and the binary.
+			</p>
+			<p>
+				Modulator markup is supported by all HTML capable browsers allowing you to spread your message 
+				across the planet, regardless of enduser device. That includes more than 6.000.000.000
+				mobilephones. The only question left is: <strong>What do you want to say?</strong>
+			</p>
 
-	<p>
-		Modulator is part of the <a href="http://parentnode.dk" target="_blank">parentNode</a> family.
-	</p>
+			<h2>Beauty of simplicity</h2>
+			<p>
+				The real beauty of this markup model is it's simplicity. It is just doing what you already know, and forgetting
+				those hacks that you have had to acquire along the way. You do not need them anymore. Modulator is really easy 
+				to implement and works with any modern frontend friendly CMS. It's simplistic and semantic structure makes it easy 
+				to read and implement. You can reuse any Modulator snippet across different websites and designs, apply 
+				functionality as needed. Modulator is perfect for both custom and template based websites.
+			</p>
+			<p>
+				 One markup, multiple presentations. This markup is for everyone and everything. It really IS that simple. 
+			</p>
 
-	<h2>Want to contribute?</h2>
-	<p>
-		We always need help. Send an email to 
-		<a href="mailto:contribute@parentnode.dk">contribute@parentnode.dk</a> to join the team.
-	</p>
+			<h2>Advantages of Modulator</h2>
+			<ul>
+				<li>Your markup works seamlessly in all HTML browsers.</li>
+				<li>Complete separation of Content, Design and Functionality.</li>
+				<li>Finish your clean HTML templates in a matter or hours, not days.</li>
+				<li>Your markup is semantic by nature.</li>
+				<li>Implementation is faster.</li>
+				<li>Frontend is detached from backend development.</li>
+				<li>Update the interface without changing your markup.</li>
+				<li>Fully <a href="http://en.wikipedia.org/wiki/HTML5" target="_blank">HTML5</a> and <a href="http://schema.org" target="_blank">Schema.org</a> compliant.</li>
+			</ul>
+
+			<p>
+				Modulator is part of the <a href="http://parentnode.dk" target="_blank">parentNode</a> family.
+			</p>
+
+			<h2>Want to contribute?</h2>
+			<p>
+				We always need help. Send an email to 
+				<a href="mailto:contribute@parentnode.dk">contribute@parentnode.dk</a> to join the team.
+			</p>
+		</div>
+	</div>
 
 </div>
--- src/templates/pages/principles.php
+++ src/templates/pages/principles.php
@@ -1,83 +1,96 @@
-<div class="scene">
-	<h1>The principles <br />of Modulator</h1>
-	<p>
-		Modulator wants to help you achive what you want, regardsless of what you want. As frontend/creative developers
-		we are faced with a multitude of challenges ranging from design idiocy, varying browser processing and rendering
-		speed, support of different devices and formfactors and restrictions imposed by predefined and often meaningless 
-		CMS output. The worst HTML is made by backend developers whom take HTML for granted. 
-	</p>
-	<p>
-		The design you are going to implement is likely to evolve, even before your current project is finished, and 
-		surely the design update scheduled next year will bring about even bigger changes. So you might as well leave 
-		it out of the equation and just think of content and semantics. CSS and JavaScript will take care of the rest.
-		Manipulator strives to make it easier for you as a frontend developer and at the same time decrease complexity
-		for the backend developer.
-	</p>
+<div class="scene principles i:scene">
 
-	<h2>Content, Design and Functionality</h2>
-	<p>
-		The idea of fully 
-		<a href="http://en.wikipedia.org/wiki/Separation_of_presentation_and_content" target="_blank">separating Content, Design and Functionality</a>
-		has been around for long. It has been considered anything from an absolute necessity to an utopian ideal. Supporters
-		are fighting for semantic content and opponents for unconstrained design. It would be nice to be able to please both
-		sides, but it is too complicated and probably just can't be done, right? Well, Modulator begs to differ.
-	</p>
-	<p>
-		Think of HTML as XML. Use it to structure your content semantically and forget about the design and interface 
-		functionality. The backend should merely work as a minimal and fully semantic content API. Use regular links to
-		connect the pieces. All of a sudden you have every bit of information available right there in your DOM ready for you
-		to build your desired interface using just JavaScript and CSS.
-	</p>
+	<div class="article" itemscope itemtype="http://schema.org/Article">
+		<h1 itemprop="name">The principles <br />of Modulator</h1>
 
-	<h2>Mobile First</h2>
-	<p>
-		Think <a href="http://www.uxmatters.com/mt/archives/2012/03/mobile-first-what-does-it-mean.php">Mobile First</a> 
-		when you structure your content - even if you are not building a mobile optimized website. 
-		Why? Because there simply isn't any reason not to. If you evade the mobile considerations, you degrade the 
-		accessibility of your content. Thinking mobile first forces you to forget the desktop design and focus on your 
-		actual content. Your markup will become simpler and more flexible. Considering the growth of mobile phones with 
-		internet access outpace desktops and tablets it will be wise to incorporate this approach sooner rather than later.
-	</p>
+		<dl class="info">
+			<dt class="published_at">Date published</dt>
+			<dd class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", filemtime(__FILE__)) ?>"><?= date("Y-m-d, H:i", filemtime(__FILE__)) ?></dd>
+			<dt class="author">Author</dt>
+			<dd class="author" itemprop="author">Martin Kæstel Nielsen</dd>
+		</dl>
 
-	<h2>Semantic HTML and SEO</h2>
-	<p>
-		Modulator implements <a href="http://schema.org" target="_blank">Schema.org</a> for extended semantics. Schema has
-		been adapted by 
-		<a href="http://en.wikipedia.org/wiki/Schema.org" target="_blank">Google, Yahoo and Microsoft</a>
-		which makes it the broadest supported semantic model available. When using Modulator markup you automatically 
-		achieve the highest level of Search engine optimization from a code point of view, but you still need to bring 
-		your own content to the party.
-	</p>
+		<div class="articlebody" itemprop="articleBody">
+			<p>
+				Modulator wants to help you achive what you want, regardsless of what you want. As frontend/creative developers
+				we are faced with a multitude of challenges ranging from design idiocy, varying browser processing and rendering
+				speed, support of different devices and formfactors and restrictions imposed by predefined and often meaningless 
+				CMS output. The worst HTML is made by backend developers whom take HTML for granted. 
+			</p>
+			<p>
+				The design you are going to implement is likely to evolve, even before your current project is finished, and 
+				surely the design update scheduled next year will bring about even bigger changes. So you might as well leave 
+				it out of the equation and just think of content and semantics. CSS and JavaScript will take care of the rest.
+				Manipulator strives to make it easier for you as a frontend developer and at the same time decrease complexity
+				for the backend developer.
+			</p>
 
-	<h2>Progressive enhancement</h2>
-	<p>
-		As a frontend programmer you might want to think of your HTML as a content database. Remember the DOM is a fully 
-		queryable data structure. Query your data, manipulate and extend. Don't overload the DOM by default. Make it 
-		fit your design using JavaScript. Progressive enhancement means modifying the DOM to extend complexity. The simpler
-		your starting point is, the easier the manipulation will be for each device segment. The more advanced your target 
-		browser is, the more manipulation it can handle. It all adds up.
-	</p>
+			<h2>Content, Design and Functionality</h2>
+			<p>
+				The idea of fully 
+				<a href="http://en.wikipedia.org/wiki/Separation_of_presentation_and_content" target="_blank">separating Content, Design and Functionality</a>
+				has been around for long. It has been considered anything from an absolute necessity to an utopian ideal. Supporters
+				are fighting for semantic content and opponents for unconstrained design. It would be nice to be able to please both
+				sides, but it is too complicated and probably just can't be done, right? Well, Modulator begs to differ.
+			</p>
+			<p>
+				Think of HTML as XML. Use it to structure your content semantically and forget about the design and interface 
+				functionality. The backend should merely work as a minimal and fully semantic content API. Use regular links to
+				connect the pieces. All of a sudden you have every bit of information available right there in your DOM ready for you
+				to build your desired interface using just JavaScript and CSS.
+			</p>
 
-	<h2>Less is more</h2>
-	<p>
-		Always keep your markup as tight as possible. This way you get the smallest and the fastest load. It really boosts
-		performance, especially on devices with less processing power. If your markup is minimal, nice and clean, querying
-		data is much faster and manipulation is actually easier to implement and read when you return for that next update.
-		Less markup means more accessibility for both Search Engines and JavaScript manipulation.
-	</p>
+			<h2>Mobile First</h2>
+			<p>
+				Think <a href="http://www.uxmatters.com/mt/archives/2012/03/mobile-first-what-does-it-mean.php">Mobile First</a> 
+				when you structure your content - even if you are not building a mobile optimized website. 
+				Why? Because there simply isn't any reason not to. If you evade the mobile considerations, you degrade the 
+				accessibility of your content. Thinking mobile first forces you to forget the desktop design and focus on your 
+				actual content. Your markup will become simpler and more flexible. Considering the growth of mobile phones with 
+				internet access outpace desktops and tablets it will be wise to incorporate this approach sooner rather than later.
+			</p>
 
-	<h2>Additional levarage</h2>
-	<p>
-		<a href="http://detector.parentnode.dk" target="_blank">Detector</a> is an easy way of
-		grouping all browsers into 9 simple target segments for performance and device optimized interfaces.
-	</p>
-	<p>
-		<a href="http://manipulator.parentnode.dk" target="_blank">Manipulator</a> is a full JavaScript library,
-		designed to work with Modulator markup and Detector segments.
-	</p>
-	<p>
-		<a href="http://janitor.parentnode.dk" target="_blank">Janitor</a> is a full PHP Content Management Framework and
-		Content Delivery Platform designed to work with Modulator, Detector and Manipulator.
-	</p>
+			<h2>Semantic HTML and SEO</h2>
+			<p>
+				Modulator implements <a href="http://schema.org" target="_blank">Schema.org</a> for extended semantics. Schema has
+				been adapted by 
+				<a href="http://en.wikipedia.org/wiki/Schema.org" target="_blank">Google, Yahoo and Microsoft</a>
+				which makes it the broadest supported semantic model available. When using Modulator markup you automatically 
+				achieve the highest level of Search engine optimization from a code point of view, but you still need to bring 
+				your own content to the party.
+			</p>
+
+			<h2>Progressive enhancement</h2>
+			<p>
+				As a frontend programmer you might want to think of your HTML as a content database. Remember the DOM is a fully 
+				queryable data structure. Query your data, manipulate and extend. Don't overload the DOM by default. Make it 
+				fit your design using JavaScript. Progressive enhancement means modifying the DOM to extend complexity. The simpler
+				your starting point is, the easier the manipulation will be for each device segment. The more advanced your target 
+				browser is, the more manipulation it can handle. It all adds up.
+			</p>
+
+			<h2>Less is more</h2>
+			<p>
+				Always keep your markup as tight as possible. This way you get the smallest and the fastest load. It really boosts
+				performance, especially on devices with less processing power. If your markup is minimal, nice and clean, querying
+				data is much faster and manipulation is actually easier to implement and read when you return for that next update.
+				Less markup means more accessibility for both Search Engines and JavaScript manipulation.
+			</p>
+
+			<h2>Additional levarage</h2>
+			<p>
+				<a href="http://detector.parentnode.dk" target="_blank">Detector</a> is an easy way of
+				grouping all browsers into 9 simple target segments for performance and device optimized interfaces.
+			</p>
+			<p>
+				<a href="http://manipulator.parentnode.dk" target="_blank">Manipulator</a> is a full JavaScript library,
+				designed to work with Modulator markup and Detector segments.
+			</p>
+			<p>
+				<a href="http://janitor.parentnode.dk" target="_blank">Janitor</a> is a full PHP Content Management Framework and
+				Content Delivery Platform designed to work with Modulator, Detector and Manipulator.
+			</p>
+		</div>
+	</div>
 
 </div>
