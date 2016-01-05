8bfc44367be5da2b53534d090d2ef7301902e227
kaestel <martin@think.dk>
2016-01-05 12:27:39 +0100

    updated semantics

--- src/templates/docs/article.php
+++ src/templates/docs/article.php
@@ -9,13 +9,14 @@
 
 <code>&lt;div itemscope itemtype=&quot;http://schema.org/Article&quot;&gt;
 	
-	&lt;h1 itemprop=&quot;name&quot;&gt;header&lt;/h1&gt;
+	&lt;h1 itemprop=&quot;headline&quot;&gt;header&lt;/h1&gt;
 	&lt;dl&gt;
 		&lt;dt class=&quot;date_published&quot;&gt;Date published&lt;/dt&gt;
 		&lt;dd class=&quot;date_published&quot; itemprop=&quot;datePublished&quot; content=&quot;2015-07-27&quot;&gt;2015-07-27&lt;/dd&gt;
 		&lt;dt class=&quot;author&quot;&gt;Author&lt;/dt&gt;
 		&lt;dd class=&quot;author&quot;itemprop=&quot;author&quot;&gt;Author of artical&lt;/dd&gt;
 	&lt;/dl&gt;
+	&lt;div itemprop=&quot;image&quot; content=&quot;<?= SITE_URL ?>/img/logo.png&quot;&gt;&lt;/div&gt;
 
 	&lt;div itemprop=&quot;articleBody&quot;&gt;
 		&lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit.&lt;/p&gt;
