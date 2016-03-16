50c6ec44fb410e229c5c3a9fad2ae0918e9dc2d1
kaestel <martin@think.dk>
2016-03-16 20:43:12 +0100

    Added license for Templator

--- src/www/js/lib/u-settings.js
+++ src/www/js/lib/u-settings.js
@@ -2,6 +2,5 @@ u.site_name = "Templator";
 u.terms_version = "terms_v1";
 u.github_fork = {"url":"https://github.com/parentnode/templator", "text":"Fork me on GitHub"};
 
-
 u.ga_account = 'UA-49741763-1';
 u.ga_domain = 'templator.parentnode.dk';
