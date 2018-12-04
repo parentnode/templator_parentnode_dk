539cfea4278dbe4111f65bf425d924f3e09d4435
threepwoodd <scope.dk@gmail.com>
2018-12-04 17:43:32 +0100

    Update includes

--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -1,4 +1,6 @@
+// Skin
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_smartphone.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/u-settings.js"></script>');
 
+// Project
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
