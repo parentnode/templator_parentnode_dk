1ce97e801f223813620a1076e07d7a67d070f03f
threepwoodd <scope.dk@gmail.com>
2018-12-04 17:45:57 +0100

    Build

--- theme/www/js/seg_seo.js
+++ theme/www/js/seg_seo.js
@@ -1,9 +1,4 @@
 /*
 Manipulator v0.9.1 Copyright 2016 http://manipulator.parentnode.dk
-asset-builder @ 2018-11-30 15:23:39
+asset-builder @ 2018-12-04 17:45:51
 */
-
-/*seg_seo_include.js*/
-
-/*seg_seo.js*/
-
