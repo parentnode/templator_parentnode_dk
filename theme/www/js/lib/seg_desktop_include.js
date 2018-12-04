a60da5f27e2f58741bd64431a77807c89d8f8916
threepwoodd <scope.dk@gmail.com>
2018-12-04 17:03:33 +0100

    Update u-settings

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,3 +1,4 @@
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/u-settings.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
