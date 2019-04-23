73e61e3e0f99485245d128ad35773d5216cef3cf
Martin Kæstel Nielsen <martin@think.dk>
2019-04-23 23:00:33 +0200

    Build and cleanup

--- theme/www/js/seg_seo.js
+++ theme/www/js/seg_seo.js
@@ -1,4 +1,9 @@
 /*
 Manipulator v0.9.1 Copyright 2016 http://manipulator.parentnode.dk
-asset-builder @ 2019-01-09 11:40:39
+asset-builder @ 2019-04-23 22:59:41
 */
+
+/*seg_seo_include.js*/
+
+/*seg_seo.js*/
+
