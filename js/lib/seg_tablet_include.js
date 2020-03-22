088c231924e37792fc7abe1512c8f977d1b539a9
Martin Kæstel Nielsen <martin@think.dk>
2020-03-22 22:52:42 +0100

    Squashed 'theme/www/assets/parentnode-skin-default/' changes from 0aeedaf..593c7db
    593c7db Correct path
    bd1899e Extended column set
    fe781ae Minor updates
    3e6d1db Remove debug output
    f00d4e5 Updates and build
    344b1cd Clean up
    1a9d8b0 Column update
    731eb7b Updates
    fab526a Build
    1a97ee7 All over the place updates
    00c539c Update manipulator
    bf289d3 cleanup
    a14a387 Reorganize and extend skin
    0a47af9 Reorganize and extend skin
    4a1cd3c Adjustments to new parentnode site
    ab1ce35 Update to new scene render model
    3e677a6 Updated include
    5039ee7 Updated includes
    358e000 Alignment
    c07f31b Added articles class to list
    2b88ded Geo location adjustment
    efcb3ab Build
    954f2b4 Skin updates
    cb320fc Updated logo
    
    git-subtree-dir: theme/www/assets/parentnode-skin-default
    git-subtree-split: 593c7dbc4df5fe8ff99da373ce2cf98e9f51881c

--- js/lib/seg_tablet_include.js
+++ js/lib/seg_tablet_include.js
@@ -5,11 +5,13 @@ document.write('<script type="text/javascript" src="/js/lib/u-basics.js"></scrip
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
 // Include BETA modules seperately
+document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-fontsready.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-notifier.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-smartphoneswitch.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-scene.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-article.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-page.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-login.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-scene.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/m-article.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/u-geolocation.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/u-checkmark.js"></script>');
--- js/manipulator/merged/seg_seo.js
+++ js/manipulator/merged/seg_seo.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.3-parentnode-skin-default Copyright 2019 https://manipulator.parentnode.dk
-js-merged @ 2019-10-13 18:55:07
+js-merged @ 2020-03-16 23:44:02
 */
 
 /*seg_seo_include.js*/
